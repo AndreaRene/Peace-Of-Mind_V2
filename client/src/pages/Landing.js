80058a858dacad2a9367f0cefa6400f2d9f98db8
Joseph Picardat <josephpicardat1@gmail.com>
2022-11-22 21:49:56 -0600

    added parts of landing page

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// import 'antd/dist/antd.css';
+// import { Row, Col } from 'antd';
+
+const LandingPage = () => {
+  return <div>Hello</div>;
+};
+
+export default LandingPage;
