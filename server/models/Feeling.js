e09e7e2d0d6bdb26b24f985f05a49f228d0f7bdd
nofutofu <aaronrweis@gmail.com>
2022-11-23 01:44:49 -0600

    baseline for feeling model

---
+++ server/models/Feeling.js
@@ -0,0 +1,57 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../../client/src/utils/dateFormat')
+
+const feelingSchema = new Schema({
+    feelingTitle: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 48,
+    },
+    feelingText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 500,
+    },
+    username: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+    },
+    hugCount: {
+        type: Number,
+    },
+    thankCount: {
+        type: Number,
+    },
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'comment',
+        },
+    ],
+    dateTime: {
+        type: Date,
+        default: Date.now,
+        get: dateFormat,
+    },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+feelingSchema
+    .virtual('commentCount')
+    .get(function () {
+        return this.comments.length;
+    });
+
+
+const Feeling = model('feeling', feelingSchema)
+
+module.exports = Feeling;
