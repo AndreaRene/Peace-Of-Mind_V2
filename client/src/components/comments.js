1897f877af178b5c0454387002c49fff99a6a978
RelentlessNC <njconklin1987@gmail.com>
2022-11-22 21:07:42 -0600

    extra push

--- client/src/components/comments.js
+++ client/src/components/comments.js
@@ -1,10 +1,14 @@
-<div>
-    <h2>Comments</h2>
-    <div>
-        {{#if comment.length}}
-        <div>
-            {{#each comment as |comment|}} {{> comment-partials comment}} {{/each}}
-        </div>
-        {{/if}}
-    </div>
-</div>
+function comments(props){
+    return(
+            <div>
+                <h2>Comments</h2>
+                <div>
+                    {{#if comment.length}}
+                    <div>
+                        {{#each comment as |comment|}} {{> comment-partials comment}} {{/each}}
+                    </div>
+                    {{/if}}
+                </div>
+            </div>
+    )
+}
