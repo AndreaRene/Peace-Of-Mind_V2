9b85d835ec7254d974bcf1f13772e9c22dd8a912
AndreaRene <andrea@interhact.net>
2022-11-26 20:34:58 -0600

    completed aboutrow component to feed about component

---
+++ client/src/pages/AboutRows.js
@@ -0,0 +1,16 @@
+import "./aboutRows.css";
+
+const AboutRows = ({ heading, text, whisper, image, sections }) => {
+  return (
+    <section className={"aboutGroup section" + (sections + 1)}>
+      <div className="aboutTexts">
+        <h2>{heading}</h2>
+        <p>{text}</p>
+        <p>{whisper}</p>
+      </div>
+      <img src={image} />
+    </section>
+  );
+};
+
+export default AboutRows;
