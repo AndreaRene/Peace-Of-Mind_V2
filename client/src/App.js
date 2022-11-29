9eb75a3adc1d9d806012ba301d319b8c642ff156
Andrea <andrea@interhact.net>
2022-11-28 20:27:28 -0600

    updating routes to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,11 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import LandingPage from './pages/Landing';
 import About from './pages/About';
 import Login from './pages/Login';
+// import Community from './pages/Community';
+// import Feeling from './pages/Feeling';
+// import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -14,13 +19,16 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/aboutPage" element={<About />} />
+        <Switch>
+          <Route path="/" element={<LandingPage />} />
+          <Switch>
+            <Route path="/about" element={<About />} />
             <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
+            {/* <Route path="/community" element={<Community />} /> */}
+            {/* <Route path="/comments" element={<Feeling />} /> */}
+            {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          </Switch>
+        </Switch>
       </Router>
     </ApolloProvider>
   );
