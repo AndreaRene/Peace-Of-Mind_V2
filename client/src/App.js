0a7415e6873912dc7f6169e41229fea55fa3573d
Andrea <andrea@interhact.net>
2022-11-28 20:40:50 -0600

    editing routing in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -19,17 +19,17 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
+        <Navbar />
         <Switch>
           <Route path="/" element={<LandingPage />} />
-          <Switch>
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            {/* <Route path="/community" element={<Community />} /> */}
-            {/* <Route path="/comments" element={<Feeling />} /> */}
-            {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          </Switch>
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/community" element={<Community />} /> */}
+          {/* <Route path="/comments" element={<Feeling />} /> */}
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         </Switch>
       </Router>
+      <Footer />
     </ApolloProvider>
   );
 }
