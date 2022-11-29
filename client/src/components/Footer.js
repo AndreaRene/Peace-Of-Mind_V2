8402ab7d98f0dfb4f8ab01bc1222b0ce7166fb25
Val Pal <valgonzalez13@yahoo.com>
2022-11-28 19:48:18 -0600

    footer styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,15 @@
-function Footer(props){
-    return(
+import './Footer.css';
+
+function Footer() {
+    return (
         <footer className="" id="pageFooter">
-        <h5 className=""></h5>
-        <p className="">24/7 Suicide and Crisis Lifeline: <a className="" href="tel:988"> 988</a></p>
-        <p className="">24/7 Suicide and Crisis Lifeline Chat: <a className=""
+            <h5 className=""></h5>
+            <p className="">24/7 Suicide and Crisis Lifeline: <a className="footerLinks" href="tel:988"> 988</a></p>
+            <p className="">24/7 Suicide and Crisis Lifeline Chat: <a className="footerLinks"
                 href="https://988lifeline.org/chat/"> Lifeline
                 Chat</a></p>
         </footer>
     )
 }
+
+export default Footer;
