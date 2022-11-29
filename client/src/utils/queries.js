4444fcd12ed7f9e66c95f66a2559fc78aa3155cc
nofutofu <aaronrweis@gmail.com>
2022-11-28 20:21:15 -0600

    added queries for graphql

---
+++ client/src/utils/queries.js
@@ -0,0 +1,72 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      gThankCount
+      gHugCount
+      rThankCount
+      rHugCount
+      feelings {
+        _id
+        feelingTitle
+        feelingText
+        username
+        hugCount
+        thankCount
+        dateTime
+        comments {
+          commentText
+          username
+          hugCount
+          thankCount
+          dateTime
+        }
+      }
+    }
+  }
+`;
+
+export const GET_SINGLE_FEELING = gql`
+  query getSingleFeeling($feelingId: ID!) {
+    feeling(feelingId: $feelingId) {
+      _id
+      feelingTitle
+      feelingText
+      username
+      hugCount
+      thankCount
+      dateTime
+      comments {
+        commentText
+        username
+        hugCount
+        thankCount
+        dateTime
+      }
+    }
+  }
+`;
+
+export const GET_FEELINGS = gql`
+  query getFeelings {
+    feelings {
+      _id
+      feelingTitle
+      feelingText
+      username
+      hugCount
+      thankCount
+      dateTime
+      comments {
+        commentText
+        username
+        hugCount
+        thankCount
+        dateTime
+      }
+    }
+  }
+`;
