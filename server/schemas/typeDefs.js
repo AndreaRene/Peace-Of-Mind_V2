918531562f52707fd7c8a26c4e18cebe9ea0a553
Nate Naranjo <naranjo_nathaniel@live.com>
2022-12-03 21:02:26 -0600

    Revamped the typeDefs, added mutations, and user feelings to the query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,37 +1,53 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql `
-type User {
-    _id: ID!
-    username: String
-    feeling: String
-    comments: String
-    gThanksCount: Int
-    gHugCount: Int
-    rThankCount: Int
-    rHugCount: Int
-}
-type Feeling {
-    feelingID: ID!
-    feelingText: String
-    dateTime: String
-    randomUsername: String
-    comment: [User]
-    hugCount: Int
-    thankCount: Int
-}
-type Comment {
-    commentID: ID!
-    commentText: String
-    dateTime: String
-    randomUsername: String
-    hugCount: Int
-    thankCount: Int
-}
-type Query {
-    user(username: String!): User
-}
+    
+    type User {
+        _id: ID!
+        username: String
+        feeling: String
+        comments: String
+        gThanksCount: Int
+        gHugCount: Int
+        rThankCount: Int
+        rHugCount: Int
+    }
+    
+    type Feeling {
+        feelingID: ID!
+        feelingText: String
+        dateTime: String
+        randomUsername: String
+        comment: [User]
+        hugCount: Int
+        thankCount: Int
+    }
+    
+    type Comment {
+        commentID: ID!
+        commentText: String
+        dateTime: String
+        randomUsername: String
+        hugCount: Int
+        thankCount: Int
+    }
+    
+    type Query {
+        users: [User]
+        user(username: String!): User
+        feelings(username: String): [Feeling]
+        feeling(feelingId: ID!): Feeling
+        me: User
+    }
 
+    type Mutation {
+        addUser(username: String!, password: String!): Auth
+        login(username: String!, password: String!): Auth
+        addFeeling(feelingText: String!): Feeling
+        addComment(feelingId: ID!, commentText: String!): Feeling
+        removeFeeling(feelingId: ID!): Feeling
+        removeComment(feelingId: ID!, commentId: ID!): Feeling
+    }
 `;
 
 module.exports = typeDefs;
