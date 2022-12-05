9269bbeef34d5b5c6068037679d6c34cc51bbd09
AndreaRene <andrea@interhact.net>
2022-12-04 20:29:50 -0600

    feeling card styling complete on single feeling page

--- client/src/pages/Feeling.js
+++ client/src/pages/Feeling.js
@@ -1,47 +1,49 @@
-import React from 'react';
-import { useQuery, useMutation } from '@apollo/client';
-import { Link } from 'react-router-dom';
-import { GET_SINGLE_FEELING } from '../utils/js/queries';
-import Thank from '../assets/icons/svg/thank-1.svg';
-import { useParams } from 'react-router-dom';
-import Icon from '@ant-design/icons';
-import { HugSvg, ThankSvg } from '../assets/icons/community-svgs';
-import CommentList from '../components/community/CommentList';
-import { Col, Row } from 'antd';
-import '../utils/css/Community.css';
+import React from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+import { GET_SINGLE_FEELING } from "../utils/js/queries";
+import { useParams } from "react-router-dom";
+import Icon from "@ant-design/icons";
+import CommentList from "../components/community/CommentList";
+import { Col, Row } from "antd";
+import "../utils/css/Community.css";
+import "../utils/css/feelingsCard.css";
+import { HugSvg, CommentSvg } from "../assets/icons/community-svgs.js";
+import Thank from "../assets/icons/svg/thank-1.svg";
+import { generateName } from "../utils/js/names";
 // import { ADD_FEELING_HUG, ADD_FEELING_THANK } from '../utils/js/mutations';
 
 const feeling = {
   _id: 234,
-  feelingTitle: 'Man this stinks',
+  feelingTitle: "Man this stinks",
   feelingText:
     "I'm all stressed out and it really stinks i hate it man it sucks gosh dangit golly gee. I'm all stressed out and it really stinks i hate it man it sucks gosh dangit golly gee. I'm all stressed out and it really stinks i hate it man it sucks gosh dangit golly gee. I'm all stressed out and it really stinks i hate it man it sucks gosh dangit golly gee. I'm all stressed out and it really stinks i hate it man it sucks gosh dangit golly gee. I'm all stressed out and it really stinks i hate it man it sucks gosh dangit golly gee. I'm all stressed out and it really stinks i hate it man it sucks gosh dangit golly gee.",
-  randomUsername: 'Celtic Butterfly',
+  randomUsername: "Celtic Butterfly",
   hugCount: 3,
   thankCount: 6,
-  dateTime: '11/30/22',
+  dateTime: "11/30/22",
   hugUsers: [667, 343, 565],
   thankUsers: [667, 343, 565],
   comments: [
     {
       _id: 588,
-      commentText: 'Awesome work dude!',
-      randomUsername: 'Pink Pufferfish',
+      commentText: "Awesome work dude!",
+      randomUsername: "Pink Pufferfish",
       hugCount: 6,
       thankCount: 5,
       hugUsers: [878, 667, 243],
       thankUsers: [667, 878, 243],
-      dateTime: '12/1/22',
+      dateTime: "12/1/22",
     },
     {
       _id: 344,
       commentText: "I'm sorry you're feeling that way :(..",
-      randomUsername: 'Bananarama Monkey',
+      randomUsername: "Bananarama Monkey",
       hugCount: 12,
       thankCount: 9,
       hugUsers: [667, 990, 123],
       thankUsers: [667, 435, 223],
-      dateTime: '12/3/22',
+      dateTime: "12/3/22",
     },
   ],
 };
@@ -69,7 +71,7 @@ const Feeling = () => {
       //       hugCount,
       //     },
       //   });
-      console.log('addedHug');
+      console.log("addedHug");
     } catch (err) {
       console.log(err);
     }
@@ -83,7 +85,7 @@ const Feeling = () => {
       //       thankCount,
       //     },
       //   });
-      console.log('addedThank');
+      console.log("addedThank");
     } catch (err) {
       console.log(err);
     }
@@ -96,10 +98,59 @@ const Feeling = () => {
   // return <div>Loading...</div>;
   //   }
   return (
-    <main className="mainContent communityWrapper">
+    <main className="communityWrapper">
       <p id="startingParagraph">Welcome, Username. It's great to see you!</p>
-      <div className="feelingWrapper">
-        <div className="eachFeelingDiv">
+      {/* <div className="feelingWrapper"> */}
+      <div className="cardWrapper">
+        {/* {feelingData.map((feeling) => ( */}
+        <div className="card">
+          <div className="cardLeft">
+            <header className="cardHeader">
+              <div className="cardTitleGroup">
+                <h2 className="cardTitle">{feeling.feelingTitle}</h2>
+                <p className="cardUser">-{generateName()}</p>
+              </div>
+              <p className="cardDate">{feeling.dateTime}</p>
+            </header>
+            <p className="cardText">{feeling.feelingText}</p>
+          </div>
+          <div className="cardRight">
+            <div className="cardIcons">
+              <div className="cardIconGroup">
+                <Link
+                  className="feelingIcon"
+                  to={`/feeling/${feeling.feelingId}`}
+                >
+                  <CommentSvg />
+                </Link>
+                <Link
+                  className="hugIcon"
+                  disabled={feeling.hugUsers?.some(
+                    (hugUserId) => hugUserId === user._id
+                  )}
+                  onClick={() =>
+                    handleAddHug(feeling.feelingId, feeling.hugCount)
+                  }
+                >
+                  <HugSvg />
+                </Link>
+              </div>
+              <Link
+                disabled={feeling.thankUsers?.some(
+                  (thankUserId) => thankUserId === user._id
+                )}
+                onClick={() =>
+                  handleAddThank(feeling.feelingId, feeling.thankCount)
+                }
+              >
+                <img src={Thank} alt="thankyou" className="thankyouIcon" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="eachFeelingDiv">
           <div className="feelingHeading">
             <Col>
               <Row>
@@ -116,7 +167,7 @@ const Feeling = () => {
                 <Link
                   className="hugIcon"
                   disabled={feeling.hugUsers?.some(
-                    hugUserId => hugUserId === user._id,
+                    (hugUserId) => hugUserId === user._id
                   )}
                   onClick={() =>
                     handleAddHug(feeling.feelingId, feeling.hugCount)
@@ -127,7 +178,7 @@ const Feeling = () => {
                 <div className="thankyouDivComment">
                   <Link
                     disabled={feeling.thankUsers?.some(
-                      thankUserId => thankUserId === user._id,
+                      (thankUserId) => thankUserId === user._id
                     )}
                     onClick={() =>
                       handleAddThank(feeling.feelingId, feeling.thankCount)
@@ -142,11 +193,11 @@ const Feeling = () => {
           <div className="feelingTextWrapper">
             <p className="feelingText">{feeling.feelingText}</p>
           </div>
-        </div>
-        <div id="comments-container">
-          <CommentList comments={feeling.comments} user={user} />
-        </div>
+        </div> */}
+      <div id="comments-container">
+        <CommentList comments={feeling.comments} user={user} />
       </div>
+      {/* </div> */}
     </main>
   );
 };
