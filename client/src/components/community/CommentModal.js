edcf042e2e129ceac41657ab4b55e7e5e6fb03d3
nofutofu <aaronrweis@gmail.com>
2022-12-05 19:08:13 -0600

    added save button and structure/css to modal

--- client/src/components/community/CommentModal.js
+++ client/src/components/community/CommentModal.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from 'antd';
 import '../../utils/css/CommentModal.css';
+import { SaveSvg } from '../../assets/icons/community-svgs';
 
 const showModal = () => {
   console.log('hi');
+};
+
+const hideModal = () => {
+  console.log('yo');
 };
 
 const CommentModal = () => {
@@ -12,10 +17,14 @@ const CommentModal = () => {
     <>
       <div className="modal-content-wrapper">
         <div className="modal-content">
-          <Link className="openModal" onClick={() => showModal()}>
+          <div className="modal-header">
+            <Link className="openModal" onClick={() => showModal()}></Link>
             Share your thoughts:
-          </Link>
-          <Form class="comment-form">
+            <Link className="saveIcon" onClick={() => hideModal()}>
+              <SaveSvg />
+            </Link>
+          </div>
+          <Form className="comment-form">
             <Form.Item name="commentText">
               <Input.TextArea rows={3} />
             </Form.Item>
